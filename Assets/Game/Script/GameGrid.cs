82bb51270dbc962b00b54cafdc9f8057d31e4465
Jakub Michalski <jakub.michalski.bukai@gmail.com>
2023-05-26 21:39:45 +0200

    Create generator map
    Add script GameGrid and 2 prefabs Grid and Agent

---
+++ Assets/Game/Script/GameGrid.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameGrid : MonoBehaviour
+{
+    public int height, width;
+
+    private NavMeshSurface navMeshSurfaces;
+
+    [SerializeField]
+    private GameObject gridPrefab;
+
+    private GameObject[,] gameGrid;
+
+    private void Awake()
+    {
+        navMeshSurfaces = gameObject.GetComponent<NavMeshSurface>();
+
+        CreatGameMap();
+
+        navMeshSurfaces.BuildNavMesh();
+    }
+
+    private void CreatGameMap()
+    {
+        gameGrid = new GameObject[height, width];
+
+        for (int z = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                gameGrid[x, z] = Instantiate(gridPrefab, new Vector3(x, 0, z), Quaternion.identity);
+                gameGrid[x, z].transform.parent = transform;
+                gameGrid[x, z].gameObject.name = "Grid Space ( x: " + x.ToString() + " , Y: " + z.ToString() + ")";
+            }
+        }
+    }
+}
